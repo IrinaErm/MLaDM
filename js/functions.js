e1cf0245ccbc2ba01e43604508b848da3668648a
IrinaErm <45590837+IrinaErm@users.noreply.github.com>
2021-03-02 18:34:10 +0400

    Final

--- js/functions.js
+++ js/functions.js
@@ -97,7 +97,85 @@ function symm() {
 		document.getElementById('symm').innerHTML = 'Симметрично';
 	}
 	else {
-		document.getElementById('symm').innerHTML = '';
+		document.getElementById('symm').innerHTML = 'Не является симметричным';
+	}
+}
+
+function antisymm() {
+	var n = document.getElementById("n").value;
+	var mas = document.getElementsByClassName("cell");
+
+	var k = 0;
+	var con = true;
+	var mas2 = [];
+	for (var i = 0; i < n; i++){
+		mas2[i] = [];
+		for (var j = 0; j < n; j++){
+			mas2[i][j] = mas[k].value;
+			k++;
+		}	
+	}
+	
+	for (var i = 0; i < n; i++){				
+		for (var j = 0; j < n; j++){
+			if(i != j) {
+				if(!(mas2[i][j] != mas2[j][i])) {
+					con = false;
+				}
+			}			
+		}	
+	}
+	
+	if(con) {
+		document.getElementById('antisymm').innerHTML = 'Кососимметрично';
+	}
+	else {
+		document.getElementById('antisymm').innerHTML = 'Не является кососимметричным';
+	}
+}
+
+function trans() {
+	var n = document.getElementById("n").value;
+	var mas = document.getElementsByClassName("cell");
+
+	var k = 0;
+	var con = true;
+	var mas2 = [];
+	var kv = [];						//перевод в двумерный массив
+	for (var i = 0; i < n; i++){
+		mas2[i] = [];
+		kv[i] = [];
+		for (var j = 0; j < n; j++){
+			mas2[i][j] = mas[k].value;
+			kv[i][j] = 0;
+			k++;
+		}	
+	}
+	
+	for (var i = 0; i < n; i++) {		//возведение матрицы в квадрат
+		for (var j = 0; j < n; j++) {
+			for (var z = 0; z < n; z++) {
+				kv[i][j] += mas2[i][z] * mas2[z][j];
+			}
+			if (kv[i][j] > 1) {
+				kv[i][j] = 1;
+			}
+		}		
+	}	
+	
+	for (var i = 0; i < n; i++){		//если единиц в квадрате матрицы больше, то нетранзитивно
+		for (var j = 0; j < n; j++){
+			if (mas2[i][j] < kv[i][j]) {
+				con = false;
+			}				
+		}		
+	}	
+	
+	if(con) {
+		document.getElementById('trans').innerHTML = 'Транзитивно';
+	}
+	else {
+		document.getElementById('trans').innerHTML = 'Не является транзитивным';
 	}
 }
 
@@ -105,4 +183,6 @@ function getRelations() {
 	check();
 	reflex();
 	symm();
+	antisymm();
+	trans();
 }
