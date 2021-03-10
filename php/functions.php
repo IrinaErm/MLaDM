c69250da5bb137772be2b07d9dbe89c013638da5
IrinaErm <45590837+IrinaErm@users.noreply.github.com>
2021-03-10 15:29:16 +0400

    Add files via upload

--- php/functions.php
+++ php/functions.php
@@ -15,33 +15,63 @@
 		//начало алгоритма
 		$distance = [];
 		$ver = [];
+		$min_ver = 0;		
+		$min_dists = [];
+		$k = 0; //кол-во вариантов путей из нач. вершины
 		for ($i = 0; $i < $n; $i++){	
-			$distance[$i] = 10000;
-			$ver[$i] = false;
-		}
-		$distance[0] = 0;	//нач. точка
-		$min_distance = 0;
-		$min_ver = 0;
-		$min_res = 0;
-		
-		while ($min_distance < 10000) {
-			$i = $min_ver;
-			$ver[$i] = true;	//окрашиваем вершину
-			for ($j = 0; $j < $n; $j++) {							//определение расстояния до ближайших вершин, запись наименьшего
-				if($distance[$i] + $mas[$i][$j] < $distance[$j]) {
-					$distance[$j] = $distance[$i] + $mas[$i][$j];
+			if ($mas[$min_ver][$i] != 10000) {				
+				for ($s = 0; $s < $n; $s++){	
+					$distance[$s] = 10000;
+					$ver[$s] = false;			
 				}
-			}
-			$min_res = $min_distance;
-			$min_distance = 10000;			//определение след. вершины
-			for ($j = 0; $j < $n; $j++) {				
-				if(!($ver[$j]) && $distance[$j] < $min_distance) {	//проверять вершины, пока есть незакрашенные
-					$min_distance = $distance[$j];	
-					$min_ver = $j;	//переходить к след. вершине, если есть связь
+				
+				$temp = $min_ver;
+				$distance[0] = 0;//$mas[$min_ver][$i];	//нач. точка
+				$min_distance = 0;
+				
+				$min_res = 0;
+				$ver_res = [];				
+				
+				while ($min_distance < 10000) {
+					$t = $temp;
+					$ver[$t] = true;	//окрашиваем вершину
+					if ($t == $min_ver) {
+						$distance[$i] = $distance[$t] + $mas[$t][$i];
+					}
+					else {
+						for ($j = 0; $j < $n; $j++) {							//определение расстояния до ближайших вершин, запись наименьшего
+							if($distance[$t] + $mas[$t][$j] < $distance[$j]) {
+								$distance[$j] = $distance[$t] + $mas[$t][$j];
+							}
+						}
+					}				
+
+					$min_res = $min_distance;
+					$min_distance = 10000;			//определение след. вершины
+					for ($j = 0; $j < $n; $j++) {
+						if ($t == $min_ver) {
+							$min_distance = $distance[$i];	
+							$temp = $i;
+							break;
+						}						
+						else if (!($ver[$j]) && $distance[$j] < $min_distance) {	//проверять вершины, пока есть незакрашенные
+							$min_distance = $distance[$j];	
+							$temp = $j;	//переходить к след. вершине, если есть связь
+						}
+					}
 				}
-			}
-		}
+				$min_dists[$k] = $min_res;
+				$k++;
+			}			
+		}	
 		
-		//print_r($min_distance);
-		echo("Длина кратчайшего пути: $min_res");
+		$min_res = $min_dists[0];
+		for($i = 1; $i < count($min_dists); $i++) {
+			if($min_dists[$i] < $min_res) {
+				$min_res = $min_dists[$i];
+			}
+		}	
+				
+		echo("Длина кратчайшего пути: $min_res <br>");
+		
 ?>
